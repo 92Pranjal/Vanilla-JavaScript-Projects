e07e6b330674f72ec7bbed689c62a4456e363ccb
pranjal <pranjalpriyadarshi01@gmail.com>
2020-04-28 19:42:45 +0530

    Primary changes

---
+++ app.js
@@ -0,0 +1,21 @@
+// console.log(document.getElementById('task-title'))
+
+// console.log(document.getElementById('task-title').id)
+// console.log(document.getElementById('task-title').className)
+// document.getElementById('task-title').textContent = "My List"
+// document.getElementById('task-title').innerText = "Task List"
+
+// document.getElementById('task-title').innerHTML = '<span style="color:red"> Task List</span>'
+
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'));
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'green';
+
+document.querySelector('li:last-child').style.color = 'yellow';
+document.querySelector('li:nth-child(3)').style.color = 'red';
+
+
+
+
